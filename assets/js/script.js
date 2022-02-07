4920220025b7e4ce75a5dc24047574f420459771
Vinicius Oliveira <vinicius.so.contato@hotmail.com>
2022-02-07 10:11:01 -0300

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+const textoHorario = document.querySelector('#texto-horario')
+const iconHorario = document.querySelector('#icon-horario')
+const nomeUsuario = document.querySelector('#nome-usuario')
+
+const modalDialog = document.querySelector('#modal-dialog')
+const btnFechar = document.querySelector('#btn-fechar')
+
+//Horario || Dashboard
+
+let data = new Date()
+let hora = data.getHours()
+if(hora >= 0 && hora <= 12) {
+    textoHorario.innerHTML += 'Bom dia,'
+    iconHorario.src = 'https://cdn-icons-png.flaticon.com/512/869/869869.png'
+}else if(hora >= 12 && hora <= 18){
+    textoHorario.innerHTML = 'Boa tarde,'
+    iconHorario.src = 'https://cdn-icons-png.flaticon.com/512/1163/1163661.png'
+}else{
+    textoHorario.innerHTML = 'Boa noite,'
+    iconHorario.src = 'https://cdn-icons-png.flaticon.com/512/6735/6735121.png'
+}
+
+nomeUsuario.innerText = localStorage.nome
+
+//Modal
+function clickModal(){
+    const modal = document.querySelector('body')
+    modal.addEventListener('click', (e) =>{
+        if(e.target.id == 'modal-dialog' || e.target.className == 'btn-fechar'){
+            modalDialog.classList.remove('mostrar')
+        }
+    })
+
+    modalDialog.classList.add('mostrar')
+}
+
+function btnCadastrarDespesa(){
+    document.getElementById('title').innerHTML = "Cadastrar Nova Despesa"
+    document.getElementById('btn-inferior').innerHTML = 'Salvar Despesa'
+    clickModal()
+}
+
+function btnFiltro(){
+    document.getElementById('title').innerHTML = "Filtro de despesas"
+    document.getElementById('btn-inferior').innerHTML = 'Procurar despesas'
+    clickModal()
+}
